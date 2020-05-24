4ac1ac8bc85113241dddc3bec9ab06890abec88d
sergi <sergi@DESKTOP-ASRN178>
2020-05-24 23:00:53 +0200

    "cambio de nombre sergio diaz"

---
+++ PracticaGitHubColaborativa/src/practicaGitHubColaborativa/SergioDiaz.java
@@ -0,0 +1,7 @@
+package practicaGitHubColaborativa;
+
+public class SergioDiaz {
+	public static String saludo = "Sergio: pp??";
+	
+	public static String despedida = "Sergio: A farmear weaponized brrr";
+}
