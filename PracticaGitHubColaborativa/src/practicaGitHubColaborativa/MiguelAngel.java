bef7d4344ec37f28d6c24094a75b51ab4283b8c1
Miguel <Miguel@DESKTOP-LU7REVS>
2020-05-24 13:12:08 +0200

    Cambio de Miguel Angel Escudero

---
+++ PracticaGitHubColaborativa/src/practicaGitHubColaborativa/MiguelAngel.java
@@ -0,0 +1,9 @@
+package practicaGitHubColaborativa;
+
+public class MiguelAngel {
+
+	public static String saludo = "Miguel: Ummm, donde esta el boton de despedida??";
+	
+	public static String despedida = "Miguel: Ah aqui estaba";
+
+}
