d0cee4d3973e80ffe496712cb6b19017859c9ad7
ruben <ruben@192.168.1.110>
2020-05-24 18:15:20 +0200

    Cambio de Ruben Oya

---
+++ PracticaGitHubColaborativa/src/practicaGitHubColaborativa/RubenOya.java
@@ -0,0 +1,7 @@
+package practicaGitHubColaborativa;
+
+public class RubenOya {
+
+	public static String saludo ="Voy a tomar un café con galletas";
+	public static String despedida ="Uf.. que cansado estoy...";
+}
--- PracticaGitHubColaborativa/src/practicaGitHubColaborativa/Saludos.java
+++ PracticaGitHubColaborativa/src/practicaGitHubColaborativa/Saludos.java
@@ -3,67 +3,72 @@ package practicaGitHubColaborativa;
 public class Saludos {
 
 	public static void main(String[] args) {
-		
+
 		// Codigo de Almu
 		System.out.println("------------------------------------------------------------------");
 		System.out.println(Almu.saludo);
 		System.out.println(Almu.despedida);
-		
-		//Codigo de Oscar
+
+		// Codigo de Oscar
 		System.out.println("------------------------------------------------------------------");
 		System.out.println(OscarDelAmo.saludo);
 		System.out.println(OscarDelAmo.despedida);
-		
-		//Codigo de Matute
+
+		// Codigo de Matute
 		System.out.println("------------------------------------------------------------------");
 		System.out.println(JavierMatute.saludo);
 		System.out.println(JavierMatute.despedida);
-		
-		//Codigo de Iván
+
+		// Codigo de Iván
 		System.out.println("------------------------------------------------------------------");
 		System.out.println(IvanMoriche.saludo);
 		System.out.println(IvanMoriche.despedida);
-		
-		//Codigo de AlexLopez
+
+		// Codigo de AlexLopez
 		System.out.println("------------------------------------------------------------------");
 		System.out.println(AlejandroLopez.saludo);
 		System.out.println(AlejandroLopez.despedida);
-		
+
 		// Codigo de Iustin
 		System.out.println("------------------------------------------------------------------");
 		System.out.println(IustinMocanu.saludo);
 		System.out.println(IustinMocanu.despedida);
-		
+
 		// Codigo de MiguelA
 		System.out.println("------------------------------------------------------------------");
 		System.out.println(MiguelAngel.saludo);
 		System.out.println(MiguelAngel.despedida);
-		
+
 		// Codigo de David
 		System.out.println("------------------------------------------------------------------");
 		System.out.println(DavidRodriguez.saludo);
 		System.out.println(DavidRodriguez.despedida);
-		
+
 		// Codigo de Cris
 		System.out.println("------------------------------------------------------------------");
 		System.out.println(CristinaHerraez.saludo);
 		System.out.println(CristinaHerraez.despedida);
-		
+
 		// Codigo de Rodrigo
 		System.out.println("------------------------------------------------------------------");
 		System.out.println(RodrigoGarcia.saludo);
 		System.out.println(RodrigoGarcia.despedida);
-		
+
 		// Codigo de Sergio Gomez
 		System.out.println("------------------------------------------------------------------");
 		System.out.println(SergioGomez.saludo);
 		System.out.println(SergioGomez.despedida);
-		
+
 		// Codigo de Carlos Moreno
 		System.out.println("------------------------------------------------------------------");
 		System.out.println(CarlosMoreno.saludo);
 		System.out.println(CarlosMoreno.despedida);
-	
+		
+		// Codigo de Ruben Oya
+		System.out.println("------------------------------------------------------------------");
+		System.out.println(RubenOya.saludo);
+		System.out.println(RubenOya.despedida);
+
 	}
 
 }
