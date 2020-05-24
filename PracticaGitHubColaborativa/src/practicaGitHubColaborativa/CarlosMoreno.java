95363f35c57f529a8ddd6f30ad098f1ba2ecbb91
lofly <lofly3@hotmail.com>
2020-05-24 17:26:55 +0200

    Cambio de Carlos Moreno

---
+++ PracticaGitHubColaborativa/src/practicaGitHubColaborativa/CarlosMoreno.java
@@ -0,0 +1,13 @@
+package practicaGitHubColaborativa;
+
+public class CarlosMoreno {
+
+	
+		
+		public static String saludo = "Carlos: El casquet volador";
+		
+		public static String despedida = "Carlos: malditos furros";
+
+
+	
+}
